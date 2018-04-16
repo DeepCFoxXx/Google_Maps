7aa93de45e9ab00136827fe0e2950fc47a5a220c
DeepCFoxXx <learncode23@gmail.com>
2018-04-16 14:44:00 +0100

    add click event added

--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -13,3 +13,20 @@ MapWrapper.prototype.addMarker = function (coords) {
   });
   return marker;
 }
+
+MapWrapper.prototype.addClickEvent = function () {
+  google.maps.event.addListener(this.googleMap, 'click', function (event) {
+    var position = { lat: event.latLng.lat(), lng: event.latLng.lng() }
+    this.addMarker(position);
+  }.bind(this));
+}
+
+MapWrapper.prototype.addInfoWindow = function (coords, text) {
+  var marker = this.addMarker(coords);
+  marker.addListener('click', function () {
+    var infoWindow = new google.maps.InfoWindow({
+      content: text
+    });
+    infoWindow.open(this.map, marker);
+  });
+}
