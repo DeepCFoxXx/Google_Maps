c92123a5bbdb75b798bd030bf5799e79ed5fe5c9
DeepCFoxXx <learncode23@gmail.com>
2018-04-16 15:01:19 +0100

    refactoring

--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -20,13 +20,3 @@ MapWrapper.prototype.addClickEvent = function () {
     this.addMarker(position);
   }.bind(this));
 }
-
-MapWrapper.prototype.addInfoWindow = function (coords, text) {
-  var marker = this.addMarker(coords);
-  marker.addListener('click', function () {
-    var infoWindow = new google.maps.InfoWindow({
-      content: text
-    });
-    infoWindow.open(this.map, marker);
-  });
-}
