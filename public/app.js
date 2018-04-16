317a887bd8c40a326557335172508069e5a79522
DeepCFoxXx <learncode23@gmail.com>
2018-04-16 15:43:26 +0100

    refactoring

--- public/app.js
+++ public/app.js
@@ -1,16 +1,16 @@
 var initialize = function(){
   var mapDiv = document.getElementById('main-map');
-  var chicagoButton = document.querySelector('#edinburgh-button');
+  var edinburghButton = document.querySelector('#edinburgh-button');
   var whereAmIButton = document.querySelector('#geo-button');
 
-  var center = { lat: 40.712784, lng: -74.005941 };
+  var center = { lat: 15.8700, lng: 100.9925 };
 
   var mainMap = new MapWrapper(mapDiv, center, 10);
   mainMap.addClickEvent();
   mainMap.addInfoWindow(center, "");
 
   var goToEdinburgh = function(){
-    var edinburgh = { lat: 55.9533, lng: 3.1883 };
+    var edinburgh = { lat: 55.9533, lng:  3.1883 };
     mainMap.googleMap.setCenter(edinburgh);
     mainMap.addInfoWindow(edinburgh, "<h3>Edinburgh</h3>");
   }
@@ -19,8 +19,8 @@ var initialize = function(){
     mainMap.geoLocate();
   }
 
-  chicagoButton.addEventListener('click', goToEdinburgh);
+  edinburghButton.addEventListener('click', goToEdinburgh);
   whereAmIButton.addEventListener('click', findLocation);
 }
 
-window.addEventListener('load', initialize);
+window.addEventListener('load', initialize);3.1883
